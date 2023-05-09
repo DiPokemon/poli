7ba444af1307798a2fd0696398750616363cb274
Evgenii Kovalenko <kov.evgenii5@gmail.com>
2023-05-09 10:48:31 +0200

    header skeleton

--- header.php
+++ header.php
@@ -10,7 +10,55 @@
         
     </head>
     <body>
-        <header>      
+        <header>
+            <div class="header__menutop">
+                <div class="header__wrapper">
+                    <nav class="menutop_nav">
+                        <ul class="menutop__list">
+                            <li><a href="#" class="links">Информация по правам пациентов</a></li>
+                            <li><a href="#" class="links">Портал ГОСУСЛУГИ</a></li>
+                            <li><a href="#" class="links">Результаты СОУТ</a></li>
+                            <li><a href="#" class="links">Противодействие коррупции</a></li>
+                        </ul>
+                        <ul class="menutop__icon">
+                        <li><a href="#" class="links"></a></li>
+                        <li><a href="#" class="links"></a></li>
+                        <li><a href="#" class="links"></a></li>
+                        <li><a href="#" class="links"></a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div class="header__contacts">
+                <div class="header__wrapper">
+                    <div class="contacts__container">
+                        <div class="contacts__container_item"></div>
+                        <div class="contacts__container_item"></div>
+                        <div class="contacts__container_item"></div>
+                        <div class="contacts__container_item"></div>
+                        <div class="contacts__container_item"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="header__menubottom">
+                <div class="header__wrapper">
+                    <nav class="menubottom_nav">
+                        <ul class="menubottom__list">
+                            <li><a href="#" class="links">Главная</a></li>
+                            <li><a href="#" class="links">О больнице</a></li>
+                            <li><a href="#" class="links">Отделение</a></li>
+                            <li><a href="#" class="links">Новости</a></li>
+                            <li><a href="#" class="links">Задать вопрос</a></li>
+                            <li><a href="#" class="links">Отзывы</a></li>
+                            <li><a href="#" class="links">Запись на приём</a></li>
+                            <li><a href="#" class="links">Контакты</a></li>
+                        
+                        </ul>
+                    </nav>
+                    
+
+                </div>
+            </div>      
                           
         </header>
     

9fddf21bd8c1ec309a2aebaa794bd61458584157
Evgenii Kovalenko <kov.evgenii5@gmail.com>
2023-05-18 12:06:51 +0200

    empty

--- functions.php
+++ functions.php
@@ -33,7 +33,7 @@ function change_logo_class( $html ) {
 
 //Register and load CSS
 function load_styles(){
-	wp_enqueue_style('fontawesome', get_template_directory_uri().'/static/fontawesome/css/fontawesome.min.css');
+	wp_enqueue_style('fontawesome', get_template_directory_uri().'/static/fontawesome/css/all.min.css');
   	wp_enqueue_style('style_min', get_template_directory_uri().'/static/css/style.min.css');
   	wp_enqueue_style('slick', get_template_directory_uri().'/static/libs/slick/slick.min.css');
   	wp_enqueue_style('slick_theme', get_template_directory_uri().'/static/libs/slick/slick-theme.min.css');

af12ac0025ef747ff8f540bcaa367fff9aec71d9
Evgenii Kovalenko <kov.evgenii5@gmail.com>
2023-06-02 13:48:08 +0200

    about, news

---
+++ about.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Template Name: Шаблон страницы О больнице
+*/
+?>
+<?php get_header(); ?>
+<p> О больнице</p>
+<?php get_footer(); ?>
---
+++ news.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Template Name: Шаблон страницы Новости
+*/
+?>
+<?php get_header(); ?>
+<p> Новости</p>
+<?php get_footer(); ?>

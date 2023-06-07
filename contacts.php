020bc30e46ea414e36e14f79bfba9f0ffe7c4f87
Evgenii Kovalenko <kov.evgenii5@gmail.com>
2023-06-07 20:05:52 +0200

    empty

--- contacts.php
+++ contacts.php
@@ -4,32 +4,64 @@ Template Name: Шаблон страницы контактов
 */
 ?>
 <?php get_header(); ?>
-<section class="page_contacts">
-<div class="page_contacts_container container">
+<section class="contacts">
+<div class="contacts_container container">
         
         <h2 class="contacts_title">
             <a href="#" class="contacts_title-link"><?= carbon_get_theme_option('contact'); ?></a>
         </h2>
         
-        <div class="page_contacts_body">
-            <div class="page_contacts_text">
-                 <div class="contacts_text_top">
-                    <p><b>Адрес:</b> 344006, г. Ростов-на-Дону, пр. Богатяновский спуск, 27/160</p>
-                    <p><b>Главный врач:</b> к.м.н., Пакус Дмитрий Игоревич</p>
-                    <p><b>Справочные:</b></p>
+        <div class="contacts_body">
+            <div class="contacts_text">
+                <div>
+                    <span class="bold dark_green"></span><?= $adress_title ?>:</span>
+                    <span><?= $address_index ?></span>
+                    <span><?= $address_city ?></span>
+                    <span><?= $address_street ?></span>
+                    <span><?= $address_build ?></span>
+                </div>
+                        
+
+                <div class="contacts_line1">
+                    <div class="adress_left"><?= carbon_get_theme_option('adress1'); ?></div>
+                    <div class="adress_right"><?= carbon_get_theme_option('adress2'); ?></div>
                 </div>
 
-                <div class="contacts_text_bottom">
-                    <ul>
-                        <li>Главного врача: 263-30-90, факс: 227-76-05, e-mail: gb4com@mail.ru</li>
-                        <li>Приемное отделение: 263-50-64</li>
-                        <li>Терапевтическое отделение 1: 8 (863) 227-75-48</li>
-                        <li>Терапевтическое отделение 2: 8 (863) 227-74-96</li>
-                        <li>Травмпункт: 263-40-23</li>
-                        <li>Отдел кадров: 227-76-05</li>
-                        
-                    </ul>
+                <div class="contacts_line2">
+                    <div class="adress_left"><?= carbon_get_theme_option('head_doctor'); ?></div>
+                    <div class="adress_right"><?=carbon_get_theme_option('name_head_doctor'); ?></div>
+                </div>
+                <div class="contacts_line3">
+                    <div class="adress_left">        
+                        <!-- Рубрику справочные  -->
+                        <a href="#" class="contacts_category-link"><?= get_category( 13, ARRAY_A)['name']; ?></a>
+                    </div>
+                </div>
+                <div class="contacts_departs">
                     
+
+                    <!-- записи -->
+                    <?php
+                $posts = get_posts([
+                    'numberposts' => -1,
+                    'category' => 13,
+                    'orderby' => 'ASC',
+                    'post_type' => 'post',
+                    'suppress_filter' => true
+                ]);
+
+
+
+                foreach ($posts as $post) {
+                    setup_postdata ($post);
+                    ?>
+                    <ul class="contacts_posts_items">
+                        <li><img class="plus_img" src="http://ce40725-wordpress-sxtw3.tw1.ru/wp-content/uploads/2023/06/photo_2023-06-06_02-54-56.png" alt="im" /><a href="#" class="contacts_posts_items-link"><?php the_title(); ?></a></li>
+                    </ul>
+                    <?php
+                }
+            wp_reset_postdata();
+            ?>
                 </div>
 
                 

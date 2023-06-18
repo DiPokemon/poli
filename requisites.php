59446328a70386d82f735b1cd40d7b644a69bedf
Evgenii Kovalenko <kov.evgenii5@gmail.com>
2023-06-18 23:37:37 +0200

    page administration

---
+++ requisites.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: Шаблон страницы Реквизиты
+*/
+?>
+<?php get_header(); ?>
+<?php include 'template-parts/variables.php' ?>
+
+
+
+
+
+
+<?php get_footer(); ?>
---
+++ vacancies.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: Шаблон страницы Вакансии
+*/
+?>
+<?php get_header(); ?>
+<?php include 'template-parts/variables.php' ?>
+
+
+
+
+
+
+<?php get_footer(); ?>

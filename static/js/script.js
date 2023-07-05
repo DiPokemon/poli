22c0488d36521f3035d621cdc1110c984ec45850
Evgenii Kovalenko <kov.evgenii5@gmail.com>
2023-07-05 20:36:33 +0200

    adaptive top_menu

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,24 @@ $(document).ready(function(){
 	$('.about_slider_top').slick({
         arrows: false,
         slidesToShow: 4,
+        responsive: [
+          {
+              breakpoint: 1200,
+              settings: {
+                arrows: false,
+                centerMode: false,
+                slidesToShow: 3,
+              }
+          },
+          {
+            breakpoint: 900,
+            settings: {
+              arrows: false,
+              centerMode: false,
+              slidesToShow: 2,
+            }
+        },
+      ]
     });
 });
 $(document).ready(function(){
@@ -22,13 +40,44 @@ $(document).ready(function(){
 	$('.slider_time_limits').slick({
         arrows: false,
         slidesToShow: 3,
+        responsive: [
+          {
+              breakpoint: 1200,
+              settings: {
+                arrows: false,
+                centerMode: false,
+                slidesToShow: 2,
+                centerPadding: '10px',
+              }
+          },
+          {
+            breakpoint: 800,
+            settings: {
+              arrows: false,
+              centerMode: false,
+              slidesToShow: 1,
+              centerPadding: '10px',
+            }
+        },
+      ]
+      
     });
 });
 
 $(document).ready(function(){
 	$('.slider_patient_routing').slick({
         arrows: false,
-        slidesToShow: 3,
+        slidesToShow: 2,
+        responsive: [
+          {
+              breakpoint: 800,
+              settings: {
+                arrows: false,
+                centerMode: false,
+                slidesToShow: 1
+              }
+          },
+      ]
   });
   
   $('.slider_free_medical').slick({

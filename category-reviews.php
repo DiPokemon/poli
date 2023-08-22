c540fd5f76bd28cbaa29e9e95f0906938aadc119
Evgenii Kovalenko <kov.evgenii5@gmail.com>
2023-08-22 21:43:24 +0200

    empty

--- category-reviews.php
+++ category-reviews.php
@@ -6,20 +6,52 @@ Template Name: Шаблон страницы Отзывы
 <?php get_header(); ?>
 <?php include 'template-parts/variables.php' ?>
 
-ЙЦУЦЙУ
-
 <section class="page_reviews">
 	<div class="page_reviews_container container">
 	   <div class="reviews_title font_w700_s60">Отзывы</div>
 	   <div class="reviews_cf7"><?php echo do_shortcode( '[contact-form-7 id="82" title="Контактная форма для страницы Отзывы"]' ); ?></div>
+		<?php
+			if ( ! function_exists( 'pagination' ) ) :
+
+				function pagination( $paged = '', $max_page = '' ) {
+					$big = 999999999; // need an unlikely integer
+					if( ! $paged ) {
+						$paged = get_query_var('paged');
+					}
+
+					if( ! $max_page ) {
+						global $wp_query;
+						$max_page = isset( $wp_query->max_num_pages ) ? $wp_query->max_num_pages : 1;
+					}
+
+					echo paginate_links( array(
+						'base'       => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+						'format'     => '?paged=%#%',
+						'current'    => max( 1, $paged ),
+						'total'      => $max_page,
+						'mid_size'   => 1,
+						'prev_text'  => __( '«' ),
+						'next_text'  => __( '»' ),
+						'type'       => 'list'
+					) );
+				}
+			endif;
+		?>
 
 		<?php
+			$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+			$args = array(
+				'paged'          => $paged, 
+				'orderby'        => 'meta_value_num', 
+				'meta_key'       => 'post_views_count', 
+				'posts_per_page' => 3
+			);
 			$reviews_posts = new WP_Query(array(
 				'category_name' => 'reviews',
 				'post_status' => 'publish',
 				'post_type' => 'post',				
-				'posts_per_page' => 7,
-				'paged' => get_query_var('paged'),				
+				'posts_per_page' => 3,
+				'paged' => get_query_var('paged') ?: 1,				
 			));
 		?>	
 
@@ -33,41 +65,16 @@ Template Name: Шаблон страницы Отзывы
 				 </div>
 
 			  <?php endwhile; ?>
-			   <?php posts_nav_link(); ?>
+			   <div class="pagination">
+				   <?php pagination( $paged, $reviews_posts->max_num_pages); // Pagination Function ?>
+			   </div>
+			   
 		   </div>
-			<?php wp_reset_query(); ?>
+			<?php wp_reset_postdata(); ?>
 		<?php endif; ?>
 
-		<!--
-	   <div class="reviews_href">
-			<div class="page1"><a href="#" class="reviews_page">1</a></div> 
-			<div class="page2"><a href="#" class="reviews_page">2</a></div>
-			<div class="page3"><a href="#" class="reviews_page">3</a></div>
-			<div class="page4"><a href="#" class="reviews_page">4</a></div>
-			<div class="page5"><a href="#" class="reviews_page">5</a></div>
-			<div class="page_next"><a href="#" class="reviews_page_next">Следующая</a></div>
-	   </div>
-		-->
+		
 	</div>
 </section>
-
-<section>
-<?php if ( have_posts() ) : 
-	while ( have_posts() ) : the_post(); ?>
-		<?php the_title()?>
-	<?php endwhile; ?>
-	
-	<?php the_posts_pagination();?>
-	
-	<?php else: ?>
-	
-	<p>Нет постов в цикле.</p>
-	
-<?php endif; ?>
-</section>
-
-
-
-
 
 <?php get_footer(); ?>

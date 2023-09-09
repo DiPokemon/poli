9abd03dab2a92c22803046cee3303e9760056d53
Evgenii Kovalenko <kov.evgenii5@gmail.com>
2023-09-09 15:39:56 +0200

    vacancies back

--- inc/custom-fields/vacancies_options.php
+++ inc/custom-fields/vacancies_options.php
@@ -8,31 +8,51 @@ Container::make('post_meta', 'Vancies page fields')
     ->add_fields(array(
         Field::make('complex', 'general_inf', 'Общая информация' )
                 ->add_fields('general_inf', 'Общая информация', array(
+                    Field::make('text','sub_title_org_name', 'Наименование юридического лица:')
+                        ->set_width(50),
                     Field::make('text','org_name', 'Наименование юридического лица:')
-                        ->set_width(33),
+                        ->set_width(50),
+                    Field::make('text', 'sub_title_addres', 'Адрес:')
+                        ->set_width(50),    
                     Field::make('text', 'addres', 'Адрес:')
-                        ->set_width(33),
+                        ->set_width(50),
+                    Field::make('text', 'sub_title_number', 'Контактный телефон:')
+                        ->set_width(50),    
                     Field::make('text', 'number', 'Контактный телефон:')
-                        ->set_width(33),
+                        ->set_width(50),
             )),
         Field::make('complex', 'vacancies', 'vacansies')
                 ->add_fields('vacansies', 'vacansies', array(
                     Field::make('text', 'title' , 'Вакансия')
-                        ->set_width(30),
+                        ->set_width(100),
+                    Field::make('text', 'sub_title_looking_for', 'Необходимое количество работников:')
+                        ->set_width(50),    
                     Field::make('text', 'looking_for', 'Необходимое количество работников:')
-                        ->set_width(30),
+                        ->set_width(50),
+                    Field::make('text','sub_title_job_type', 'Характер работы: ')
+                        ->set_width(50),    
                     Field::make('text','job_type', 'Характер работы: ')
-                        ->set_width(30),
+                        ->set_width(50),
+                    Field::make('text', 'sub_title_salary', 'Заработная плата: ')
+                        ->set_width(50),
                     Field::make('text', 'salary', 'Заработная плата: ')
-                        ->set_width(30),
+                        ->set_width(50),
+                    Field::make('text', 'sub_title_schedule', 'Режим работы: ')
+                        ->set_width(50),    
                     Field::make('text', 'schedule', 'Режим работы: ')
-                        ->set_width(30),
+                        ->set_width(50),
                     Field::make('text', 'qualification', 'требования')
-                        ->set_width(30),
+                        ->set_width(100),
                 )),  
+
         Field::make('complex', 'bottom_inf', 'information')
                 ->add_fields('bottom_inf', 'bottom_inf',array(
+                    Field::make('text', 'sub_title1_hr', 'Начальник отдела кадров')
+                    ->set_width(33),
+                    Field::make('text', 'sub_title2_hr', 'Начальник отдела кадров')
+                    ->set_width(33),
                     Field::make('text', 'hr', 'Начальник отдела кадров')
+                    ->set_width(33),
                 ))
 
     ))
--- vacancies.php
+++ vacancies.php
@@ -13,27 +13,28 @@ Template Name: Шаблон страницы Вакансии
         <h2 class="vacancies_title font_w700_s60"><?php the_title(); ?></h2>
         <?php foreach ($general_inf as $general) :?>
         <div class="vacancies_contact" >
-            <div class="vacancies_contact_item  block1"><p class="vacancies_contact_item_top">Наименование юридического лица:</p> <span class="vacancies_contact_item_down"><?=$general['org_name']?></span></div>
-            <div class="vacancies_contact_item  block2" ><span class="vacancies_contact_item_top">Адрес:</span><span class="vacancies_contact_item_down"><?=$general['addres']?></span></div>
-            <div class="vacancies_contact_item  block3"><p class="vacancies_contact_item_top">Контактный телефон:</p><span class="vacancies_contact_item_down"><?=$general['number']?></span></div>  
+            <div class="vacancies_contact_item  block1"><p class="vacancies_contact_item_top"><?=$general['sub_title_org_name']?></p> <span class="vacancies_contact_item_down"><?=$general['org_name']?></span></div>
+            <div class="vacancies_contact_item  block2" ><span class="vacancies_contact_item_top"><?=$general['sub_title_addres']?></span><span class="vacancies_contact_item_down"><?=$general['addres']?></span></div>
+            <div class="vacancies_contact_item  block3"><p class="vacancies_contact_item_top"><?=$general['sub_title_number']?></p><span class="vacancies_contact_item_down"><?=$general['number']?></span></div>  
         </div>
         <?php endforeach ;?>
         <div class="vacancies_block">
             <?php foreach ($vacancies as $vacancie) :?>
             <div class="vacancies_block_item">
                 <h3 class="vacancies_block_title"><?=$vacancie['title']?></h3>
-                <div class="vacancies_block_text"><b>Необходимое количество работников: </b><?=$vacancie['looking_for']?><br>
-                    <b>Характер работы: </b><?=$vacancie['job_type']?><br>
-                    <b>Заработная плата: </b><?=$vacancie['salary']?><br>
-                    <b>Режим работы: </b><?=$vacancie['schedule']?><br>
+                <div class="vacancies_block_text"><b><?=$vacancie['sub_title_looking_for']?></b><?=$vacancie['looking_for']?><br>
+                    <b><?=$vacancie['sub_title_job_type']?> </b><?=$vacancie['job_type']?><br>
+                    <b><?=$vacancie['sub_title_salary']?></b><?=$vacancie['salary']?><br>
+                    <b><?=$vacancie['sub_title_schedule']?> </b><?=$vacancie['schedule']?><br>
                     <b><?=$vacancie['qualification']?></div>
                 </div>
                 <?php endforeach ;?>
         </div>
         <div class="vacancies_data">
-            <p class="font_w400_s18">«26» декабря 2022 г.</p>
+            
                 <?php foreach($bottom_inf as $inf) :?>
-            <p class="font_w400_s18">Начальник отдела кадров ____________<?=$inf['hr']?></p>
+            <p class="font_w400_s18"><?=$inf['sub_title1_hr']?></p>        
+            <p class="font_w400_s18"><?=$inf['sub_title2_hr']?><?=$inf['hr']?></p>
                 <?php endforeach ;?>
         </div>
     </div>

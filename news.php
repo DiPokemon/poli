4955e38b9e96295e27fa4cd6d0f2ebde5ff054d7
Timur-tiger <timahusamov@mail.ru>
2023-09-12 22:55:42 +0300

    поиск

--- news.php
+++ news.php
@@ -25,11 +25,13 @@ Template Name: Шаблон страницы Новости
                         <img src="http://test-dev1.ru/wp-content/uploads/otdelenie/Vector.png"
                             alt="Search" class="search-icon">
                     </button>
-                    <input type="text" placeholder="Поиск" class="search-input">
+                    <!-- <input type="text" placeholder="Поиск" class="search-input"> -->
+                    <?= do_shortcode('[ivory-search id="288" title="AJAX Search Form"]'); ?>
                 </div>
             </div>
 
-
+            
+            
 
 
 
